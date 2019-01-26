b083741e227c9c815f119de6b00723823b5d7f3a
kodoj <kodoj1@o2.pl>
2019-01-26 22:27:11 +0100

    dequeue works, peek method red stage

--- src/main/java/com/codecool/queue/FIFOQueue.java
+++ src/main/java/com/codecool/queue/FIFOQueue.java
@@ -1,4 +1,45 @@
 package com.codecool.queue;
 
 public class FIFOQueue {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public void enqueue(String data) {
+        if(length == 0) {
+            head = new Node(data);
+            tail = head;
+            length++;
+        } else {
+            Node current = head;
+
+            while(current.next != null) {
+                current = current.next;
+            }
+            current.next = new Node(data);
+            current.next.previous = current;
+            length++;
+        }
+    }
+
+
+    public String dequeue() {
+        String response = "";
+        if(length == 0) {
+            return response;
+        } else if (length == 1){
+            response = head.toString();
+            head = null;
+        } else {
+            response = head.toString();
+            head.next.previous = null;
+            head = head.next;
+            length--;
+        }
+        return response;
+    }
+
+
+
 }
--- src/test/java/com/codecool/queue/FIFOQueueTest.java
+++ src/test/java/com/codecool/queue/FIFOQueueTest.java
@@ -7,9 +7,35 @@ import static org.junit.jupiter.api.Assertions.*;
 public class FIFOQueueTest {
 
     @Test
-    void createQueueWithOneElement() {
+    void createQueue() {
         FIFOQueue fifoQueue = new FIFOQueue();
-        fifoQueue.enqueue("hello");
         assertNotNull(fifoQueue);
+    }
+
+    @Test
+    void testDequeueWithOneElement() {
+        FIFOQueue fifoQueue = new FIFOQueue();
+        String word = "hello";
+        fifoQueue.enqueue(word);
+        assertEquals(word, fifoQueue.dequeue());
+    }
+
+    @Test
+    void dequeueWithFewElements() {
+        FIFOQueue fifoQueue = new FIFOQueue();
+        String word = "hello";
+        fifoQueue.enqueue(word);
+        fifoQueue.enqueue("world");
+        fifoQueue.enqueue("kodoj");
+        fifoQueue.enqueue("here");
+        assertEquals(word, fifoQueue.dequeue());
+    }
+
+    @Test
+    void peekTest() {
+        FIFOQueue fifoQueue = new FIFOQueue();
+        String word = "hello";
+        fifoQueue.enqueue(word);
+        assertEquals(word, fifoQueue.peek());
     }
 }
