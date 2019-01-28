36027121f9dc78a024fd87b396644da01bad6efd
kodoj <kodoj1@o2.pl>
2019-01-28 14:36:10 +0100

    minHeap folders and classes created

---
+++ src/main/java/com/codecool/heap/minHeap.java
@@ -0,0 +1,4 @@
+package com.codecool.heap;
+
+public class minHeap   {
+}
---
+++ src/test/java/com/codecool/heap/minHeapTest.java
@@ -0,0 +1,7 @@
+package com.codecool.heap;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class minHeapTest {
+}
