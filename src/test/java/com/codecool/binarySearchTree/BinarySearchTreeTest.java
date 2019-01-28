f37ec77c884f9539cff50ba1f841b4120d21aecb
kodoj <kodoj1@o2.pl>
2019-01-28 13:59:04 +0100

    array to binary search tree works as expected

--- src/test/java/com/codecool/binarySearchTree/BinarySearchTreeTest.java
+++ src/test/java/com/codecool/binarySearchTree/BinarySearchTreeTest.java
@@ -36,10 +36,27 @@ public class BinarySearchTreeTest {
     }
 
     @Test
-    void containsRetrnFalseAfterElementIsDeleted() {
+    void containsReturnFalseAfterElementIsDeleted() {
         BinarySearchTree binarySearchTree = new BinarySearchTree(10);
         binarySearchTree.insert(8);
         binarySearchTree.remove(8);
         assertFalse(binarySearchTree.contains(8));
+    }
+
+    @Test
+    void arrayToBinarySearchTree() {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        int length = array.length;
+        int binarySearchTreeStartPoint = array[length / 2];
+        BinarySearchTree binarySearchTree = new BinarySearchTree(binarySearchTreeStartPoint);
+        int[] newArray = new int[length - 1];
+        System.arraycopy(array, 0, newArray, 0, binarySearchTreeStartPoint - 1);
+        System.arraycopy(array, binarySearchTreeStartPoint + 1, newArray, binarySearchTreeStartPoint - 1, length - (binarySearchTreeStartPoint + 1));
+        int newArrayLength = newArray.length;
+        for (int i = 0; i < newArrayLength; i++) {
+            binarySearchTree.insert(newArray[i]);
+        }
+
+        assertTrue(binarySearchTree.contains(11));
     }
 }
