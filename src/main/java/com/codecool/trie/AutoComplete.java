751a61833e2e035368cdff2c5e9c7d2a3961bc5a
kodoj <kodoj1@o2.pl>
2019-02-06 14:39:01 +0100

    initial structure for autocomplete Trie

---
+++ src/main/java/com/codecool/trie/AutoComplete.java
@@ -0,0 +1,23 @@
+package com.codecool.trie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AutoComplete {
+
+    public void addWord(String wordToAdd) {
+    }
+
+
+    public List<String> autoComplete(String baseChars) {
+        List<String> words = new ArrayList<>();
+
+        return words;
+    }
+
+
+    public boolean removeWord(String wordToRemove) {
+
+        return false;
+    }
+}
---
+++ src/main/java/com/codecool/trie/TrieNode.java
@@ -0,0 +1,4 @@
+package com.codecool.trie;
+
+public class TrieNode {
+}
