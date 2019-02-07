077e0171609a36c9010801d323236be620ed55d3
kodoj <kodoj1@o2.pl>
2019-02-07 11:38:48 +0100

    you can now add strings to hashTable, which are checking if there is enough free space, if not, resize itself

--- src/main/java/com/codecool/spellchecker/HashTable.java
+++ src/main/java/com/codecool/spellchecker/HashTable.java
@@ -1,4 +1,62 @@
 package com.codecool.spellchecker;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class HashTable {
+
+    String[] wordlist;
+    int length;
+    int currentWordsCounter = 0;
+    HashFunction hashFunction;
+
+    public HashTable(int length) {
+        this.length = length;
+        wordlist = new String[length];
+        hashFunction = new HashFunction();
+    }
+
+
+    public void add(String word) {
+        int index = hashFunction.hashString(length, word);
+        boolean added = false;
+        while(!added) {
+            if (wordlist[index].equals("")) {
+                wordlist[index] = word;
+                currentWordsCounter++;
+                added = true;
+            } else {
+                index++;
+            }
+        }
+        hashTableMaintenance();
+    }
+
+
+    private void hashTableMaintenance() {
+        if(moreThan80PercentOfLength()) {
+            resizeArray();
+        }
+    }
+
+
+    private boolean moreThan80PercentOfLength() {
+        final int MAX_PERCENT_OF_OCCUPIED_BUCKETS = 80;
+        if((currentWordsCounter / length) * 100 > MAX_PERCENT_OF_OCCUPIED_BUCKETS) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+    private void resizeArray() {
+        length = length + (length / 4);
+        String[] newArray = new String[length];
+        List<String> currentlyStoredWords = Arrays.asList(wordlist);
+        hashFunction.putListOfStringsToHashTable(currentlyStoredWords, newArray);
+        wordlist = newArray;
+    }
 }
